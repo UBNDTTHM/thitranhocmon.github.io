75c0162c21c2bad108dc10e8c40633a7de0dbc00
UBNDTTHM <phanthanhhieu1990hcm@gmail.com>
2025-03-26 09:41:37 +0700

    Create firebase-Config.js

---
+++ firebase-Config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
